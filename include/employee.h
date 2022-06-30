b8b75275c9411363bcbdaa96385b1a440267873a
lebed <lebed@DESKTOP-BKIICKL>
2022-06-30 15:20:38 +0300

    Add project files.

---
+++ include/employee.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+
+
+
+enum Task
+{
+    A,
+    B,
+    C,
+    AMOUNT_TASK,
+    NONE
+};
+
+
+
+class Employee
+{
+public:
+
+    void addSubordinate()
+    {
+        Employee* newWorker = new Employee;
+        newWorker->boss = this;
+        subordinate.push_back(newWorker);
+    }
+
+    int getAmountSubordinate()
+    {
+        return subordinate.size();
+    }
+
+    Employee* goSubordinateAt(const int index)
+    {
+        return subordinate[index];
+    }
+
+    void setTask()
+    {
+        if (this->task == NONE)
+        {
+            int task = rand() % AMOUNT_TASK;
+            this->task = static_cast<Task>(task);
+        }
+    }
+
+    Task getTask()
+    {
+        return task;
+    }
+
+private:
+
+    Employee* boss = nullptr;
+    std::vector <Employee*> subordinate;
+    Task task = NONE;
+};
---
+++ src/company.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include "employee.h"
+
+int main()
+{
+    Employee* company = new Employee;
+    std::cout << "Enter amount of teams: ";
+    int amountTeams;
+    std::cin >> amountTeams;
+    for (int i = 0; i < amountTeams; ++i)
+    {
+        company->addSubordinate();
+        std::cout << "Enter amount of workers on " << i + 1 << " team: ";
+        int amountWorkers;
+        std::cin >> amountWorkers;
+        Employee* manager = company->goSubordinateAt(i);
+        for (int j = 0; j < amountWorkers; ++j)
+        {
+            manager->addSubordinate();
+        }   
+    }
+    system("cls");
+
+    amountTeams = company->getAmountSubordinate();
+    for (int i = 0; i < amountTeams; ++i)
+    {
+        std::cout << "Enter assignment for " << i + 1 << " team: ";
+        int assignment;
+        std::cin >> assignment;
+
+        std::srand(assignment + i);
+        Employee* manager = company->goSubordinateAt(i);
+        int amountTask = manager->getAmountSubordinate();
+        std::cout << "the manager of team " << i + 1 << " received task " << assignment << " from the boss" << std::endl;
+        for (int i = 0; i < amountTask; ++i)
+        {
+            manager->setTask();
+            if (manager->getTask() == 0)
+            {
+                std::cout << "employee " << assignment << "." << i << " received a type A task" << std::endl;
+            }
+            if (manager->getTask() == 1)
+            {
+                std::cout << "employee " << assignment << "." << i << " received a type B task" << std::endl;
+            }
+            if (manager->getTask() == 2)
+            {
+                std::cout << "employee " << assignment << "." << i << " received a type C task" << std::endl;
+            }    
+        }
+        std::cout << std::endl;
+    }      
+}
